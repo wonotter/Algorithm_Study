cca7a4c246d51219ee1444534dd1a2ab14b4b885
Wonho Kim <138003446+wonotter@users.noreply.github.com>
2025-03-11 11:45:03 +0900

    [Gold V] Title: 신기한 소수, Time: 756 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+    }
+
+    static boolean isPrime(int num) {
+        if (num == 2) return true;
+        else if (num < 2) return false;
+        else if (num % 2 == 0) return false;
+
+        for (int i = 3; i <= num / 2; i++) {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void DFS(int num, int count) {
+        if (count == N) {
+            if (isPrime(num)) {
+                System.out.println(num);
+            }
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) continue;
+
+            if (isPrime(num * 10 + i)) {
+                DFS(num * 10 + i, count + 1);
+            }
+        }
+    }
+}
