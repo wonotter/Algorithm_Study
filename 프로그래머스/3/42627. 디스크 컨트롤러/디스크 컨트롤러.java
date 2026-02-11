21f211b4ed476abe551292034cccc077ca92dba0
Wonho Kim <wonho.kim99@gmail.com>
2026-02-11 11:41:28 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.58 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -5,54 +5,27 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-        PriorityQueue<Jobs> pq = new PriorityQueue<>();
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         
         int idx = 0;
         int time = jobs[0][0];
         
         while (idx < jobs.length || !pq.isEmpty()) {
             while (idx < jobs.length && jobs[idx][0] <= time) {
-                pq.add(new Jobs(idx, jobs[idx][0], jobs[idx][1]));
-                idx++;
+                pq.add(jobs[idx++]);
             }
             
             if (pq.isEmpty()) {
                 time = jobs[idx][0];
-                pq.add(new Jobs(idx, jobs[idx][0], jobs[idx][1]));
-                idx++;
+                pq.add(jobs[idx++]);
             }
             
-            Jobs job = pq.poll();
-            time += job.duration;
-            answer += time - job.requestTime;
+            int[] job = pq.poll();
+            time += job[1];
+            answer += time - job[0];
         }
         
         return answer / jobs.length;
-    }
-}
-
-class Jobs implements Comparable<Jobs> {
-    
-    int jobNumber;
-    int requestTime;
-    int duration;
-    
-    public Jobs(int jobNumber, int requestTime, int duration) {
-        this.jobNumber = jobNumber;
-        this.requestTime = requestTime;
-        this.duration = duration;
-    }
-    
-    @Override
-    public int compareTo(Jobs o) {
-        int c = Integer.compare(this.duration, o.duration);
-        if (c != 0)
-            return c;
-        
-        c = Integer.compare(this.requestTime, o.requestTime);
-        if (c != 0)
-            return c;
-        
-        return Integer.compare(this.jobNumber, o.jobNumber);
     }
 }
