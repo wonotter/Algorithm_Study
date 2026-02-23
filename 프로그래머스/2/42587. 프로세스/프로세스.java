21e6063a4ba89d317a922314b3edd5663e286dd8
Wonho Kim <wonho.kim99@gmail.com>
2026-02-23 21:45:56 +0900

    [level 2] Title: 프로세스, Time: 2.02 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<Node> queue = new LinkedList<>();
+        
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new Node(priorities[i], i));
+        }
+        
+        int count = 0;
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            
+            boolean hasHigher = false;
+            for (Node next : queue) {
+                if (cur.priority < next.priority) {
+                    hasHigher = true;
+                    break;
+                }
+            }
+            
+            if (hasHigher) {
+                queue.add(cur);
+            } else {
+                count++;
+                
+                if (cur.idx == location) {
+                    return count;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
+
+class Node {
+    int priority;
+    int idx;
+    
+    public Node(int priority, int idx) {
+        this.priority = priority;
+        this.idx = idx;
+    }
+}
