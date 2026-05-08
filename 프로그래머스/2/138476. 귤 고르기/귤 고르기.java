26bee52204ce2666bb06783e74cd7c5b8b95d584
Wonho Kim <wonho.kim99@gmail.com>
2026-05-08 22:29:00 +0900

    [level 2] Title: 귤 고르기, Time: 45.95 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,60 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        Arrays.sort(tangerine);
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-        PriorityQueue<Tangerine> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1.amount == o2.amount) {
-                return o1.amount - o2.amount;
-            }
-            
-            return o2.amount - o1.amount;
-        });
-        
-        int flag = tangerine[0];
-        int count = 0;
-        for (int i = 0; i < tangerine.length; i++) {
-            if (flag == tangerine[i]) {
-                count++;
-            } else {
-                pq.add(new Tangerine(tangerine[i - 1], count));
-                
-                flag = tangerine[i];
-                count = 1;
-            }
+        for (int one : tangerine) {
+            map.put(one, map.getOrDefault(one, 0) + 1);
         }
         
-        pq.add(new Tangerine(tangerine[tangerine.length - 1], count));
-        
-        Tangerine top = pq.peek();
-        if (top.amount > k) {
-            return 1;
-        }
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
         
         int answer = 0;
-        while (k > 0) {
-            Tangerine oneType = pq.poll();
-            int oneAmount = oneType.amount;
-            
-            k -= oneAmount;
+        for (int key : list) {
+            int size = map.get(key);
+            k -= size;
             answer++;
+            
+            if (k <= 0) {
+                break;
+            }
         }
         
         return answer;
-    }
-}
-
-class Tangerine {
-    int size;
-    int amount;
-    
-    public Tangerine(int size, int amount) {
-        this.size = size;
-        this.amount = amount;
-    }
-    
-    @Override
-    public String toString() {
-        return "size=" + size + "," + "amount=" + amount;
     }
 }
