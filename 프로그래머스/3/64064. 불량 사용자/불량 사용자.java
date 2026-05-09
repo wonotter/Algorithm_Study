a6e8c3481c1b62a8dff74544420040e7c82007bb
Wonho Kim <wonho.kim99@gmail.com>
2026-05-09 23:04:32 +0900

    [level 3] Title: 불량 사용자, Time: 12.74 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,52 +1,42 @@
 import java.util.*;
 
 class Solution {
-    static Set<String> result = new HashSet<>();
-    static boolean[] visited;
+    static Set<Integer> result = new HashSet<>();
     
     public int solution(String[] user_id, String[] banned_id) {
-        visited = new boolean[user_id.length];
-        
-        DFS(0, new ArrayList<>(), user_id, banned_id);
+        DFS(0, 0, user_id, banned_id);
         
         return result.size();
     }
     
-    public static void DFS(int depth, List<String> selected, String[] user_id, String[] banned_id) {
+    public static void DFS(int depth, int bit, String[] user_id, String[] banned_id) {
+        // 모든 banned_id 처리
         if (depth == banned_id.length) {
-            // 순서 제거를 위해 정렬
-            List<String> temp = new ArrayList<>(selected);
-            Collections.sort(temp);
-            
-            // 하나의 문자열로 저장
-            String key = String.join(",", temp);
-            
-            result.add(key);
+            result.add(bit);
             return;
         }
         
-        // banned_id 1개씩 순서대로 들고오기
         String banned = banned_id[depth];
         
         for (int i = 0; i < user_id.length; i++) {
-            if (visited[i]) {
+            
+            // ex. bit = 0 1 0 0 1, i = 0
+            // 0 1 0 0 1 & 0 0 0 0 1 == 0 0 0 0 1 이므로
+            // & 연산 결과가 0이 아니면 i는 이미 선택한 user라는 의미
+            if ((bit & (1 << i)) != 0) {
                 continue;
             }
             
-            // 패턴이 일치하지 않는 경우
             if (!match(user_id[i], banned)) {
                 continue;
             }
             
-            // 방문 처리
-            visited[i] = true;
-            selected.add(user_id[i]);
-            
-            DFS(depth + 1, selected, user_id, banned_id);
-            
-            // 백트래킹
-            visited[i] = false;
-            selected.remove(selected.size() - 1);
+            // 값에 의한 복사로 비트를 전달하기 때문에 백트래킹 과정이 필요 없음
+            // ex. bit = 0, depth = 0 => DFS(0, 0) 상태 
+            // => DFS(0 + 1, 0 | (1 << 0))
+            // => DFS(1, 1) // 2번째 1은 값 복사로 넘어감
+            // DFS 호출이 끝나면 다시 DFS(0, 0) 상태로 돌아온다는 뜻
+            DFS(depth + 1, bit | (1 << i), user_id, banned_id);
         }
     }
     
